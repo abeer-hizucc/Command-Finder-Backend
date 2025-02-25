cae82d7a65932c2ed91549f85365e563475f42e0
abeer-hizucc <marksmanabr@gmail.com>
2025-02-25 14:47:26 +0600

    bug fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,9 +26,6 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
     {
       provide:APP_GUARD,
       useClass:ThrottlerGuard
-    },
-    {
-      provide:AP
     }
   ],
 })
